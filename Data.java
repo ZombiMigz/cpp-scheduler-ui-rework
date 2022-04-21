c0cee7814d0bb0847f0d208ddd62921eb5bc6e8e
Luciano Lim <lmlim@cpp.edu>
2022-04-20 18:03:40 -0700

    adds class adding/displaying

--- Data.java
+++ Data.java
@@ -1,37 +1,41 @@
 public class Data {
   public static class Section {
-    public Section(String term, double avgGPA, String courseLabel, String instructor, int classNumber, String days,
+    public Section(String term, double avgGPA, int totalEnrollment, String courseName, String instructor,
+        int classNumber, String days,
         String time,
-        String location, String component, double units, int section) {
+        String location, String mode, double units, int section) {
       this.term = term;
       this.avgGPA = avgGPA;
-      this.courseLabel = courseLabel;
+      this.courseName = courseName;
       this.instructor = instructor;
       this.classNumber = classNumber;
       this.days = days;
       this.time = time;
       this.location = location;
-      this.component = component;
+      this.mode = mode;
       this.units = units;
       this.section = section;
+      this.totalEnrollment = totalEnrollment;
     }
 
     String term;
     double avgGPA;
-    String courseLabel;
+    int totalEnrollment;
+    String courseName;
     String instructor;
     int classNumber;
     String days;
     String time;
     String location;
-    String component;
+    String mode;
     double units;
     int section;
+
   }
 
-  public static Section[] sectionList = { new Section("Spring 2022", 3.56, "CS 2450", "Ben Steichen", 33066, "MW",
+  public static Section[] sectionList = { new Section("Spring 2022", 3.56, 70, "CS 2450", "Ben Steichen", 33066, "MW",
       "2:30PM-3:45PM", "Bldg 8 Rm 348", "Fully Synchronous", 3, 1),
-      new Section("Spring 2022", 3.411, "CS 4250", "Ben Steichen", 33098, "MW", "4:00PM-5:15PM", "Bldg 8 Rm 302",
+      new Section("Spring 2022", 3.411, 55, "CS 4250", "Ben Steichen", 33098, "MW", "4:00PM-5:15PM", "Bldg 8 Rm 302",
           "Fully Synchronous", 3, 1) };
 
   public static String[] courseList = { "CS 2450", "CS 4250" };
--- Settings.java
+++ Settings.java
@@ -1,15 +1,99 @@
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class Settings {
-  private Pane root;
+  private VBox root;
+  // private ArrayList<Course> courseList = new ArrayList<>();
+  private VBox classList = new VBox();
+
+  public Pane getAddClass() {
+    Label label = new Label("Add Class");
+    label.getStyleClass().add("settingLabel");
+
+    // Term Select
+    Label termLabel = new Label("Term");
+    ChoiceBox<String> termChoices = new ChoiceBox<>();
+    termChoices.getItems().addAll("Fall 2021", "Spring 2022", "Fall 2022");
+    // Default value
+    termChoices.setValue("Fall 2022");
+
+    // Course Select
+    Label courseSelectLabel = new Label("Course");
+    ChoiceBox<String> courseSelect = new ChoiceBox<>();
+    courseSelect.getItems().addAll(Data.courseList);
+    courseSelect.setValue(Data.courseList[0]);
+
+    // Add Button
+    Button button = new Button("Add Course");
+    button.setOnAction(e -> {
+      addCourse(new Course(courseSelect.getValue()));
+    });
+
+    return new VBox(label, termLabel, termChoices, courseSelectLabel, courseSelect, button);
+  }
+
+  public Pane getClassList() {
+    Label label = new Label("Class List");
+    label.getStyleClass().add("settingLabel");
+    classList.getChildren().add(label);
+
+    return classList;
+  }
+
+  private void addCourse(Course course) {
+    VBox sectionList = new VBox();
+    TitledPane coursePane = new TitledPane(course.courseName, sectionList);
+
+    for (Data.Section section : course.sections) {
+      String sectionTitle = section.section + " - " + section.instructor + " - " + section.mode + " | " + "GPA:"
+          + section.avgGPA + "(" + section.totalEnrollment + ")";
+      VBox sectionData = new VBox();
+      TitledPane sectionPane = new TitledPane(sectionTitle, sectionData);
+
+      sectionData.getChildren().addAll(new Label("Class Number: " + section.classNumber),
+          new Label("Days: " + section.days), new Label("Time: " + section.time),
+          new Label("Location: " + section.location), new Label("Units: " + section.units));
+      sectionList.getChildren().add(sectionPane);
+    }
+
+    this.classList.getChildren().add(coursePane);
+  }
 
   public Settings() {
-    root = new Pane(new Label(
-        "Settings"));
+    root = new VBox();
+    root.setId("settings");
+
+    Label label = new Label("Settings");
+    label.setId("settingsLabel");
+
+    root.getChildren().addAll(label, getAddClass(), getClassList());
+
+    // Class List change listener
+
   }
 
   public Pane getView() {
     return root;
+  }
+
+  private class Course {
+    public Course(String courseName) {
+      this.courseName = courseName;
+      ArrayList<Data.Section> sections = new ArrayList<>();
+      for (Data.Section section : Data.sectionList) {
+        if (section.courseName.equals(courseName))
+          sections.add(section);
+      }
+      this.sections = sections.toArray(new Data.Section[0]);
+    }
+
+    String courseName;
+    Data.Section[] sections;
   }
 }
